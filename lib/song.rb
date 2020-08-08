1ba8b399f9b53b4874fcbfc76f784cd38a5a0716
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-08 20:56:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ def self.artist_count
   artist_count = {}
   @@artists.each do |artist|
     if artist_count[artist]==nil
-      artist_count=1
+      artist_count[artist]=1
     else
       artist_count[artist]+=1
     end
